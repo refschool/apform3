9d3545477a32ad3ff78c7cf08349cb985a1c1e3f
Yvon Huynh <yvon.huynh@gmail.com>
2020-12-10 14:41:57 +0100

    add category formType

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryFormType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,19 +31,22 @@ class CategoryController extends AbstractController
     /**
      * @Route("/category/add",name="ajoutCategory")
      */
-    public function addCategory()
+    public function addCategory(Request $request, EntityManagerInterface $em)
     {
-        $em = $this->getDoctrine()->getManager();
-
         $category = new Category;
-        $category->setName('Xiaomi')->setDescription('Un autre fabricant de smartphone')
-            ->setSlug('xiaomi');
+        $form = $this->createForm(CategoryFormType::class, $category);
 
-        $em->persist($category);
+        $form->handleRequest($request);
 
-        $em->flush();
-        //dd('fait');
 
-        return $this->render('success.html.twig', []);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('success');
+        }
+
+
+        return $this->render('category/add.html.twig', ['form' => $form->createView()]);
     }
 }
