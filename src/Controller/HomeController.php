8790cc9046954826c872129beaf9efcd29a59529
Yvon Huynh <yvon.huynh@gmail.com>
2021-01-21 08:28:02 +0100

    add logger

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Entity\Category;
+use Psr\Log\LoggerInterface;
 use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -18,10 +19,16 @@ class HomeController extends AbstractController
      * Lister tous les produit toute catégorie confondue
      * @Route("/",name="index")
      *  */
-    public function index(EntityManagerInterface $em)
+    public function index(EntityManagerInterface $em, LoggerInterface $logger)
     {
         //Lister les produits
         $listeProduits = $em->getRepository(Product::class)->findAll();
+        $logger->info("triggerred homepage");
+        $logger->error('An error occurred');
+        $logger->critical('I left the oven on!', [
+
+            'cause' => 'in_hurry',
+        ]);
 
         return $this->render('index.html.twig', ['listeProduits' => $listeProduits]);
     }
